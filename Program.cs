fb06cb72dd023c5bef01e610fe8fac0bfe4a2a42
Pavel Sadykov <2439747@gmail.com>
2023-06-14 20:59:44 +0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.IO;
+
+namespace Задание_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string typeName = typeof(long).FullName;
+            string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string cookiesPath = Environment.GetFolderPath(Environment.SpecialFolder.Cookies);
+
+            string output = $"Тип: {typeName}\n" +
+                            $"Папка 'Документы': {documentsPath}\n" +
+                            $"Папка 'Cookies': {cookiesPath}";
+
+            File.WriteAllText("output.txt", output);
+
+            Console.WriteLine("Результаты сохранены в файл 'output.txt'.");
+        }
+    }
+}
+
